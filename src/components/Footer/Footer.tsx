e0b147e1b97020e9946edfdc9526eca02c86ea31
404Nikhil <nicksd111@gmail.com>
2023-10-22 22:42:26 +0530

    cheap_fix_scroll

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,26 +2,24 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-white bottom-0 left-0 w-full h-42 py-2 px-4">
-
-      <div className='bg-[#212121] p-4 w-full h-full text-white rounded-3xl'>
+    <div className="bg-white w-[1510px] bottom-0 py-2">
+      <div className='bg-[#212121] p-4 text-white rounded-3xl'>
         <div className='ml-12'>
-        <div className='text-white text-[34px]'>
+          <div className='text-white text-[34px]'>
             Anbv
-        </div>
-        <div className='text-[#D9D9D9] text-base'>
+          </div>
+          <div className='text-[#D9D9D9] text-base'>
             @2023 OSS Club All
-        </div>
-        <div className='text-[#D9D9D9] text-base'>
+          </div>
+          <div className='text-[#D9D9D9] text-base'>
             Rights reserved
-        </div>
+          </div>
         </div>
         <div className='flex justify-center'>
-        Made with ❤️ by OSS Club
-        </div>
+          Made with ❤️ by OSS Club
         </div>
       </div>
-
+    </div>
   );
 };
 
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -12,7 +12,7 @@ export default function HomeScreen() {
   }
 
   return (<>
-    <div className="flex h-screen flex-col justify-center items-center ">
+    <div className="flex h-screen w-full flex-col justify-center items-center ">
       <div className='flex h-screen flex-col gap-4 justify-center items-center'>
         <h1 className="text-8xl text-slate-900 flex justify-center font-bold">Anubhav</h1>
         <p className="text-4xl text-slate-800 flex justify-center">Stories of Success</p>
