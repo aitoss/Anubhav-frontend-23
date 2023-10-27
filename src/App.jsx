fe29c5b3cd468e2460d3c6356352bb8c42dd8e93
Jatin Pandey <jatinpandey212@gmail.com>
2023-10-27 21:59:19 +0530

    Small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
