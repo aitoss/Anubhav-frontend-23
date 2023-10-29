18ebc4906ecc42c01f84c5a851cb3e180e5351e7
Jatin Pandey <jatinpandey212@gmail.com>
2023-10-29 05:34:46 +0530

    routing v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import HomeScreen from './pages/HomeScreen'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
@@ -7,12 +7,16 @@ import BlogSection from './components/BlogSection/BlogSection';
 
 const App = () => {
   return (
-    <div className="flex flex-col">
+    <Routes>
+      <Route path='/' element={
+      <div className="flex flex-col">
       <Navbar />
       <HomeScreen />
       <BlogSection/>
       <Footer />
     </div>
+      } />
+    </Routes>
   );
 }
 
