391b1d7ab175cc45a435e43b05558a7236821e15
Jatin Pandey <jatinpandey212@gmail.com>
2023-10-31 02:22:31 +0530

    added-bg

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo1 from '../assets/images/user1.png';
 import logo2 from '../assets/images/user2.png';
 import logo3 from '../assets/images/user3.png';
+import background from '../assets/bg.png';
 import adobe from '../assets/company logo/adobe.svg';
 import amazon from '../assets/company logo/amazon.svg';
 import atlassian from '../assets/company logo/atlassian.svg';
@@ -21,10 +22,10 @@ export default function HomeScreen() {
   }
 
   return (<>
-    <div className="flex h-screen w-full flex-col justify-center items-center">
+    <div className="flex h-screen w-full flex-col justify-center items-center" style={{backgroundImage: `url(${background})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
       <div className='flex h-screen flex-col gap-4 justify-center items-center'>
         <h1 className="text-6xl lg:text-8xl text-slate-900 flex justify-center font-bold">Anubhav</h1>
-        <p className="text-3xl lg:text-4xl text-slate-800 flex justify-center">Stories of Success</p>
+        <p className="text-3xl lg:text-4xl text-[#414141] flex justify-center">Stories of Success</p>
         <div className='flex items-center gap-4'>
           <div className='flex'>
             <img src={logo2} alt="random" className="w-12 mr-[-16px] rounded-full border-4 border-white" />
@@ -42,7 +43,11 @@ export default function HomeScreen() {
             onChange={onChange}
           />
           <div className="absolute inset-y-0 right-0 pr-4 flex items-center pointer-events-none">
-            <svg fill="#9ca3af" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z" /></svg>
+            <svg width="24" height="24" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15.8333 28.6509C22.8289 28.6509 28.5 22.9798 28.5 15.9842C28.5 8.9886 22.8289 3.31754 15.8333 3.31754C8.83769 3.31754 3.16663 8.9886 3.16663 15.9842C3.16663 22.9798 8.83769 28.6509 15.8333 28.6509Z" stroke="#FFD700" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M29.8333 29.9842L27.1666 27.3175" stroke="#FFD700" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+
           </div>
         </div>
       </div>
