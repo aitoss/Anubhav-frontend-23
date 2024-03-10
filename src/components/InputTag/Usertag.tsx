ea3433d4b25b5e8afcf690cbb0e631ef949f9c41
Harshal141 <harshalmukundapatil@gmail.com>
2024-03-10 15:20:09 +0530

    Add tag suggestions and company suggestions to InputTag component

--- src/components/InputTag/Usertag.tsx
+++ src/components/InputTag/Usertag.tsx
@@ -1,11 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 import { RxCross1 } from "react-icons/rx";
 import { FiArrowRight } from "react-icons/fi";
+import { BACKEND_URL } from "../../constants";
+import './style.css'
 
 //commit
-const Inputtag = () => {
+const Inputtag = ({ setTags, tags }) => {
+
+  useEffect(() => {
+    const fetchTagSuggestions = async () => {
+      try {
+        const response = await axios.get(BACKEND_URL+'/tags');
+        setTagSuggestions(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.error('Error fetching tag suggestions:', error);
+      }
+    };
+
+    fetchTagSuggestions();
+  }, []);
+
+  const [tagSuggestions, setTagSuggestions] = useState([]);
+
   const [tag, setTag] = useState("");
-  const [tags, setTags] = useState([]);
 
   const handleChange = (e) => {
     setTag(e.target.value);
@@ -59,12 +79,18 @@ const Inputtag = () => {
           <input
             type="text"
             name="tag"
+            list="tagSuggestions"
             className="w-full rounded-lg text-md bg-white border-[1px] shadow-sm shadow-[#00000020] ring ring-transparent border-[#78788033] p-3 text-[#3C3C43]  placeholder:text-[#3C3C4399] focus:outline-none focus:placeholder:text-[#3c3c4350] md:w-full sm:p-2 sm:text-[13px]"
             placeholder="Tags relevant to your field"
             value={tag}
             onChange={handleChange}
             onKeyDown={handleKeyDown}
           />
+           <datalist id="tagSuggestions">
+              {tagSuggestions.map((suggestion, index) => (
+                <option key={index} value={suggestion._id} />
+              ))}
+            </datalist>
         </div>
       </div>
     </div>
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const BACKEND_URL = 'https://oss-backend.vercel.app/api/anubhav';
