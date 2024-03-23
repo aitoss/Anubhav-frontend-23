6e22a8085e6c71e8baac4d5fa2f8490c592d15ae
Lokendra Kushwah <118094744+Lokendrakushwah12@users.noreply.github.com>
2024-03-23 12:07:19 +0530

    mobile nav

---
+++ src/components/Navbar/MobileNav.jsx
@@ -0,0 +1,49 @@
+import { React, useState, useEffect } from 'react'
+import { NavLink, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import './Navbar.css';
+
+const MobileNav = ({ isOpen }) => {
+    // const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        console.log(isOpen);
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'scroll';
+        }
+    }, []);
+
+    return (
+        <>
+            <motion.div
+                className='w-full'
+                initial={{ opacity: 0, translateY: -10 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                exit={{ opacity: 0, translateY: -10 }}
+                transition={{ duration: 0.15, delay: 0.0 }}
+            >
+                <div className={`z-30 fixed bg-[#ffffffdd] bg-blur w-full shadow-lg shadow-[rgba(0,0,0,0.07)]`}>
+                    <div className="flex flex-col justify-center  border-b pb-2">
+                        <NavLink className=" p-4 text-center hover:bg-[#00000012]" to="/videos">
+                            Videos
+                        </NavLink>
+                        <NavLink className=" p-4 text-center hover:bg-[#00000012]" to="/request">
+                            Request Article
+                        </NavLink>
+                        <Link to="/create" className="px-3 text-[20px] text-[#212121] font-[500]  p-1 cursor-pointer hover:text-[#313131] ">
+                            <div className="flex gap-2 py-2 px-2 justify-center items-center text-[20px] bg-[#212121] rounded-lg text-[#fff] font-[400]   p-2 cursor-pointer hover:bg-[#313131] hover:focus:outline:none hover:focus:border:none">
+                                Write Article
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 10 19" fill="none">
+                                    <path d="M1.91003 17.171L8.43003 10.651C9.20003 9.88103 9.20003 8.62103 8.43003 7.85103L1.91003 1.33103" stroke="#fff" strokeWidth="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </motion.div>
+        </>
+    )
+}
+
+export default MobileNav
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { Cross as Hamburger } from 'hamburger-react'
-import logo from '../../assets/images/logo.svg';
 import { FaTimes } from "react-icons/fa";
 import { CiMenuFries } from "react-icons/ci";
 import { motion } from 'framer-motion';
+import logo from '../../assets/images/logo.svg';
+import MobileNav from './MobileNav';
 import './Navbar.css';
 
 const Navbar = () => {
-    const [isOpen, setOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false) //for hamburger menu
+    const [MobileNavOpen, setMobileNavOpen] = useState(false);
+
+    const handleClick = () => {
+        setMobileNavOpen(!MobileNavOpen);
+        setIsOpen(!isOpen);
+    };
+
     const scrollToBlog = () => {
         const blogSection = document.getElementById('blog-section');
         if (blogSection) {
@@ -16,27 +24,25 @@ const Navbar = () => {
         }
     };
 
-    const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
-    const content =
-        <>
-            <div className="lg:hidden bg-[white] left-0 h-screen w-screen flex flex-col items-center justify-center gap-8 absolute transition duration-300 x-md:w-[100%]">
-                {/* <NavLink to="/videos">
-                Videos
-            </NavLink> */}
-                <NavLink to="/request">
-                    Request Article
-                </NavLink>
-                <Link to="/create" className="text-[20px] text-[#212121] font-[500]  p-1 cursor-pointer hover:text-[#313131] ">
-                    <div className="flex gap-2 py-1 px-2 justify-center items-center text-[20px] bg-[#212121] rounded-lg text-[#fff] font-[400]   p-2 cursor-pointer hover:bg-[#313131] hover:focus:outline:none hover:focus:border:none">
-                        Write Article
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 10 19" fill="none">
-                            <path d="M1.91003 17.171L8.43003 10.651C9.20003 9.88103 9.20003 8.62103 8.43003 7.85103L1.91003 1.33103" stroke="#fff" strokeWidth="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                </Link>
-            </div>
-        </>
+    // const content =
+    //     <>
+    //         <div className="lg:hidden bg-[white] left-0 h-screen w-screen flex flex-col items-center justify-center gap-8 absolute transition duration-300 x-md:w-[100%]">
+    //             <NavLink to="/videos">
+    //                 Videos
+    //             </NavLink>
+    //             <NavLink to="/request">
+    //                 Request Article
+    //             </NavLink>
+    //             <Link to="/create" className="text-[20px] text-[#212121] font-[500]  p-1 cursor-pointer hover:text-[#313131] ">
+    //                 <div className="flex gap-2 py-1 px-2 justify-center items-center text-[20px] bg-[#212121] rounded-lg text-[#fff] font-[400]   p-2 cursor-pointer hover:bg-[#313131] hover:focus:outline:none hover:focus:border:none">
+    //                     Write Article
+    //                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 10 19" fill="none">
+    //                         <path d="M1.91003 17.171L8.43003 10.651C9.20003 9.88103 9.20003 8.62103 8.43003 7.85103L1.91003 1.33103" stroke="#fff" strokeWidth="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+    //                     </svg>
+    //                 </div>
+    //             </Link>
+    //         </div>
+    //     </>
 
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true);
@@ -56,22 +62,24 @@ const Navbar = () => {
         };
     }, [prevScrollPos]);
 
-    const navClasses = `bg-[#ffffffcc] bg-blur border-b flex item-center justify-center h-[70px] p-3 lg:flex w-screen text-black fixed top-0 z-10 transition-transform transform ${visible ? 'translate-y-0' : '-translate-y-full'
+    const navClasses = `bg-[#ffffffcc] bg-blur border-b flex item-center justify-center h-[70px] p-3 lg:flex w-screen text-black z-40 transition-transform transform ${visible ? 'translate-y-0' : '-translate-y-full'
         }`;
 
     return (
         <>
-            <motion.div
-                initial={{ opacity: 0, translateY: 0 }}
-                animate={{ opacity: 1, translateY: 0 }}
-                exit={{ opacity: 0, translateY: 100 }}
-                transition={{ duration: 0.15, delay: 0.05 }}
-            >
+            {/* <div className="flex flex-col"> */}
 
-                <nav className={navClasses} aria-label="Global">
+            <nav className={navClasses} aria-label="Global">
+                <motion.div
+                    className='w-full'
+                    initial={{ opacity: 0, translateY: 0 }}
+                    animate={{ opacity: 1, translateY: 0 }}
+                    exit={{ opacity: 0, translateY: 100 }}
+                    transition={{ duration: 0.15, delay: 0.05 }}
+                >
 
                     <div className="flex md:hidden w-full h-full justify-between items-center max-w-[1400px] m-auto">
-                        <div className="flex w-full items-center justify-between px-8 md:px-0">
+                        <div className="flex w-full items-center justify-between lg:px-8">
                             <Link to="/" className="p-2">
                                 <img className="" src={logo} alt="" />
                             </Link>
@@ -108,19 +116,21 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div>
-                        {click && content}
+                        {/* {click && content} */}
                     </div>
-                    <div className="hidden md:flex w-full items-center justify-between px-8 md:px-0">
+                    <div className="hidden md:flex w-full items-center justify-between px-0">
                         <Link to="/" className="md:flex hidden p-2">
                             <img className="" src={logo} alt="" />
                         </Link>
                         <div className="items-center justify-center p-0 m-0 z-50 md:flex hidden text-white transition-all" onClick={handleClick}>
                             {/* {click ? <FaTimes /> : <CiMenuFries />} */}
-                            <Hamburger direction="right" color="#212121" size={32} toggled={isOpen} toggle={setOpen} />
+                            <Hamburger direction="right" color="#212121" size={32} toggled={isOpen} toggle={setIsOpen} />
                         </div>
                     </div>
-                </nav>
-            </motion.div>
+                </motion.div>
+            </nav>
+            {MobileNavOpen && <MobileNav isOpen={isOpen} />}
+            {/* </div> */}
         </>
     );
 };
