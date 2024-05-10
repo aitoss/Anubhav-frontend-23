c5edff807bb4490e7895424f962e3d0c9ac789bd
Gourav2609 <herculeswarrior.in@gmail.com>
2024-05-10 23:57:53 +0530

    updating branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,61 @@
-import { Route, Routes } from 'react-router-dom';
-import HomeScreen from './pages/HomeScreen'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer';
-import Create from './pages/Create';
-import Error404 from './pages/Error404';
-import ViewBlog from './pages/ViewBlog';
-import BlogSection from './components/BlogSection/BlogSection';
-import Videos from './pages/Videos';
-import Guidelines from './pages/Guidelines';
-import RequestArticle from './pages/RequestArticle';
-import TermsService from './pages/TermsService';
-import Video from "./pages/Videos"
+import { Route, Routes } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Create from "./pages/Create";
+import Error404 from "./pages/Error404";
+import ViewBlog from "./pages/ViewBlog";
+import BlogSection from "./components/BlogSection/BlogSection";
+import Videos from "./pages/Videos";
+import Guidelines from "./pages/Guidelines";
+import RequestArticle from "./pages/RequestArticle";
+import TermsService from "./pages/TermsService";
+import Video from "./pages/Videos";
+import Logo from "./components/Loader/DummyLoader";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route path='/' element={
-        <div className="flex flex-col mx-auto overflow-hidden">
-          <Navbar />
-          <HomeScreen />
-          <div className="flex flex-col items-center">
-            <BlogSection />
-            <Video />
-            <Footer />
-          </div>
-        </div>
-      } />
-      <Route path='/Create' element={<Create />} />
-      <Route path='/blog/:id' element={<ViewBlog />} />
+      <Route
+        path="/"
+        element={
+          <>
+            {loading ? (
+              <div className="w-full flex justify-center h-screen items-center">
+                <Logo />
+              </div>
+            ) : (
+              <div className="flex flex-col mx-auto overflow-hidden">
+                <Navbar />
+                <HomeScreen />
+                <div className="flex flex-col items-center">
+                  <BlogSection />
+                  <Video />
+                  <Footer />
+                </div>
+              </div>
+            )}
+          </>
+        }
+      />
+      <Route path="/Create" element={<Create />} />
+      <Route path="/blog/:id" element={<ViewBlog />} />
       {/* <Route path='/videos' element={<Videos />} /> */}
-      <Route path='/guidelines' element={<Guidelines />} />
-      <Route path='/request' element={<RequestArticle />} />
+      <Route path="/guidelines" element={<Guidelines />} />
+      <Route path="/request" element={<RequestArticle />} />
       <Route path="/TermService" element={<TermsService />} />
-      <Route path='*' element={<Error404 />} />
+      <Route path="*" element={<Error404 />} />
     </Routes>
   );
-}
+};
 
-
-export default App
+export default App;
