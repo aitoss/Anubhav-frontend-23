55fc962b066f8f30e61db70b2f3b6d44c4c18391
404Nikhil <nicksd111@gmail.com>
2024-06-15 22:35:59 +0530

    footer issue fix(requestArticle.jsx)

--- src/pages/RequestArticle.jsx
+++ src/pages/RequestArticle.jsx
@@ -76,22 +76,21 @@ const RequestArticle = () => {
 
   return (
     <>
-    
-   <div className="flex flex-col h-screen">
-   <Navbar />
-      <div className="h-16 md:h-8"></div>
-      <div
-        className="flex flex-col mx-auto overflow-hidden mt-6 justify-center gap-3 items-center"
-        style={{ backgroundImage: `url(${background2})` }}
-      >
+           <Navbar />
+      <div className="flex flex-col min-h-screen justify-center items-center">
+        <div className="flex-grow  flex">
+          <div
+            className="flex flex-col mx-auto overflow-hidden mt-6 justify-center gap-3 items-center"
+            style={{ backgroundImage: `url(${background2})` }}
+          >
         {/* basic info */}
-        <div className=" relative w-[100%] max-w-[90%] flex  justify-center lg:w-[50%] py-7">
+        <div className=" w-[100%]  flex items-center  justify-center  py-7">
           <form
             onSubmit={(e) => {
               e.preventDefault();
               handleSubmit();
             }}
-            className="relative w-[100%] max-w-[1400px] rounded-xl border-[1px]  bg-white p-7 pb-4 flex flex-col gap-3  shadow-lg shadow-[rgba(0,0,0,0.05)] md:gap-1  md:w-full md:p-5"
+            className=" w-full max-w-[1600px] rounded-xl border-[1px]  bg-white p-7 pb-4 flex flex-col gap-3  shadow-lg shadow-[rgba(0,0,0,0.05)] md:gap-1  md:w-full md:p-5"
           >
             <div className="w-full">
               <motion.div
@@ -278,58 +277,17 @@ const RequestArticle = () => {
             </motion.div>
           </form>
         </div>
-        <Footer />
       </div>
-      <ErrorMessage error={error} />
-      <SuccessMessage
-        requestSend={requestSend}
-        setRequestSend={setRequestSend}
-      />
-      {/* {requestError && (
-        <div
-          id="toast-danger"
-          class="flex fixed bottom-4 right-4 items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
-          role="alert"
-        >
-          <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
-            </svg>
-            <span class="sr-only">Error icon</span>
-          </div>
-          <div class="ms-3 text-sm font-normal">Error Sending The Request.</div>
-          <button
-            type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            onClick={() => setRequestError(false)}
-            aria-label="Close"
-          >
-            <span class="sr-only">Close</span>
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                strokeWidth="2"
-                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-              />
-            </svg>
-          </button>
+      </div>
+          <ErrorMessage error={error} />
+          <SuccessMessage
+            requestSend={requestSend}
+            setRequestSend={setRequestSend}
+          />
         </div>
-      )} */}
-   </div>
+        <div className="mt-auto">
+          <Footer />
+        </div>
     </>
   );
 };
