6766e079100c594d6478cd8d2fb9223723ba756d
404Nikhil <nicksd111@gmail.com>
2024-08-06 19:55:40 +0530

    fix

--- src/components/BlogSection/Blog.jsx
+++ src/components/BlogSection/Blog.jsx
@@ -11,17 +11,20 @@ import BlogCardLoading from "./BlogCardLoading";
 
 import { BACKEND_URL } from "../../constants";
 import { formatDate, ReadTime } from "../../services/date";
-
+import BlogLoading from "./BlogLoading";
+import MinuteReadLikes from "../MinuteReadLikes/MinuteReadLikes";
 const Blog = () => {
   const { id } = useParams();
   const [blogData, setBlogData] = useState([]);
   const [similarArticles, setSimilarArticles] = useState(null);
   const [timeStamp, setTimeStamp] = useState("");
   const [readingTime, setReadingTime] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   const fetchBlogData = async () => {
     const response = await axios.get(BACKEND_URL + "/blog/" + id);
     setBlogData(response.data);
+    setLoading(false);
     setTimeStamp(formatDate(response.data.createdAt));
     const article = response.data;
     setReadingTime(ReadTime(article.description));
@@ -56,71 +59,74 @@ const Blog = () => {
     window.scrollTo(0, 0);
   }, [id]);
 
+
+
   return (
-    <div className="container items-center lg:p-6 mx-auto lg:mx-auto lg:w-[65%] lg:px-20 p-5">
-      <br />
-      <br />
-      <br />
-      <div className="data w- items-start lg:justify-start justify-center flex-col lg:p-4 space-y-2 md:mt-0  ">
-        <div className="heading">
-          <a
-            className="text-2xl lg:text-5xl font-bold text-gray-700 hover:text-gray-800"
-            href="/link"
-          >
-            {blogData?.title}
-          </a>
-        </div>
-        <Author
-          person={{
-            name: blogData?.author?.name,
-            company: blogData?.companyName,
-          }}
-        />
-        <Tags data={blogData?.articleTags}></Tags>
-        <div className="flex pb-4 lg:gap-10 items-center">
-          <p className="text-gray-500">{`${readingTime} mins read • ${timeStamp}`}</p>
-          <div className="flex gap-3 ml-auto">
-            <a href="#">
-              <CiHeart color="#888888" />
-            </a>
-            <a href="#">
-              <CiBookmark color="#888888" />
-            </a>
-          </div>
-        </div>
-        <div className="lorem-container text-black py-3 flex flex-col items-center justify-center">
-          {blogData.imageUrl != "your_image_url_here" && (
-            <div className="lg:h-[400px] lg:pb-10 flex flex-col items-center justify-center">
-              <img
-                src={blogData?.imageUrl}
-                className="w-full lg:h-full"
-                alt=""
-              />
+    <>
+      {loading == true ? <BlogLoading /> :
+
+        <div className="container items-center lg:p-6 mx-auto lg:mx-auto lg:w-[65%] lg:px-20 p-5">
+          <br />
+          <br />
+          <br />
+          <div className="data w- items-start lg:justify-start justify-center flex-col lg:p-4 space-y-2 md:mt-0  ">
+            <div className="heading">
+              <a
+                className="x-sm:text-3xl text-4xl lg:text-5xl tracking-tighter font-bold text-[#212121]"
+              // href="/link"
+              >
+                {blogData?.title}
+              </a>
             </div>
-          )}
-          <div className="w-full text-[18px] bg-white shadow-none rounded-lg">
-            <ReactQuill
+            <Author
+              person={{
+                name: blogData?.author?.name,
+                company: blogData?.companyName,
+              }}
+            />
+            <Tags data={blogData?.articleTags}></Tags>
+            <MinuteReadLikes
+              id={id} // Pass the blog id to MinuteReadLikes
+              readingTime={readingTime}
+              timeStamp={timeStamp}
+            />
+            <div className="lorem-container text-black py-3 flex flex-col items-center justify-center">
+              {blogData.imageUrl !== "your_image_url_here" && (
+                <div className=" lg:pb-10 w-full flex flex-col items-center justify-center">
+                  <img
+                    src={blogData?.imageUrl}
+                    className="lg:h-[300px] lg:w-[750px] h-[400px] x-sm:h-[200px] md:h-[300px] w-full object-cover rounded-lg"
+                    alt=""
+                  />
+                </div>
+              )}
+              <div className="w-full text-[18px] bg-white shadow-none rounded-lg">
+              <ReactQuill
               value={blogData?.description}
               theme="bubble"
               readOnly
               className="w-full h-full"
             />
-          </div>
-        </div>
-      </div>
+              </div>
+            </div>
 
-      {similarArticles ? (
-        <Articles similarArticles={similarArticles} /> // Render Articles component when similarArticles is not null
-      ) : (
-        <>
-          <BlogCardLoading />
-          <BlogCardLoading />
-          <BlogCardLoading />
-          <BlogCardLoading />
-          <BlogCardLoading />
-        </>
-      )}
-    </div>
+
+          </div>
+
+          {similarArticles ? (
+            <Articles similarArticles={similarArticles} /> // Render Articles component when similarArticles is not null
+          ) : (
+            <>
+              <BlogCardLoading />
+              <BlogCardLoading />
+              <BlogCardLoading />
+              <BlogCardLoading />
+              <BlogCardLoading />
+            </>
+          )}
+        </div>
+      }
+    </>
   );
 };
 
