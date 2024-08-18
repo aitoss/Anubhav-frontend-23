d063e66d5b1a7d20de517ad5b29e1d6d6306ffe5
404Nikhil <nicksd111@gmail.com>
2024-08-18 15:14:16 +0530

    mobile fix

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -219,26 +219,26 @@ const Search = ({ mode, focus, full }) => {
                   .slice(0)
                   .reverse()
                   .map((search, index) => (
-                    <div className={`px-4 py-2 flex flex-row w-full justify-between items-center ${hoverClass}`} key={index}>
+                    <div className="flex flex-row w-full justify-between items-center px-4 py-2" key={index}>
+  <div className="flex justify-start items-center gap-2 cursor-pointer" onClick={() => handleSuggestionClick(search)}>
+    <svg
+      className="w-5 h-5"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="#9a9a9a"
+    >
+      <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12H4C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C9.25022 4 6.82447 5.38734 5.38451 7.50024L8 7.5V9.5H2V3.5H4L3.99989 5.99918C5.82434 3.57075 8.72873 2 12 2ZM13 7L12.9998 11.585L16.2426 14.8284L14.8284 16.2426L10.9998 12.413L11 7H13Z"></path>
+    </svg>
+    <div className="flex-grow truncate">{search}</div>
+  </div>
+  <span
+    className="hover:underline hover:cursor-pointer"
+    onClick={() => handleRemove(recentSearches.length - index - 1)}
+  >
+    <X size={16} />
+  </span>
+</div>
 
-                      <div className="flex justify-center items-center gap-2 cursor-pointer" onClick={() => handleSuggestionClick(search)}>
-                        <svg
-                          className="w-[20px]"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="#9a9a9a"
-                        >
-                          <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12H4C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C9.25022 4 6.82447 5.38734 5.38451 7.50024L8 7.5V9.5H2V3.5H4L3.99989 5.99918C5.82434 3.57075 8.72873 2 12 2ZM13 7L12.9998 11.585L16.2426 14.8284L14.8284 16.2426L10.9998 12.413L11 7H13Z"></path>
-                        </svg>
-                        <div className=" w-[400px]">{search}</div>
-                      </div>
-                      <span
-                        className="hover:underline hover:cursor-pointer"
-                        onClick={() => handleRemove(recentSearches.length - index - 1)}
-                      >
-                        <X size={16} />
-                      </span>
-                    </div>
                   ))}
               </>
             )}
