49d43ac3e81b318d3b6ea2747f9e0a08d1c7a234
404Nikhil <nicksd111@gmail.com>
2024-09-27 02:02:01 +0530

    footer and fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,88 +7,89 @@ import { SparklesCore } from "./Sparkles";
 
 const Footer = () => {
 
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   const Glow = () => {
     return (
-      <>
-        <svg width="1328" height="295" viewBox="0 0 1328 295" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g clip-path="url(#clip0_816_14847)">
-            <g opacity="0.2" filter="url(#filter0_f_816_14847)">
-              <path d="M1023.78 -631.5L1043.93 -611.347L690.376 -257.794L443.723 -23L670.223 -277.947L1023.78 -631.5Z" fill="url(#paint0_linear_816_14847)" />
-            </g>
-            <g opacity="0.32" filter="url(#filter1_f_816_14847)">
-              <path d="M855.777 -623L875.929 -602.847L522.376 -249.294L275.723 -14.5L502.223 -269.447L855.777 -623Z" fill="url(#paint1_linear_816_14847)" />
-            </g>
-            <g opacity="0.2" filter="url(#filter2_f_816_14847)">
-              <path d="M-358.348 757.929L-378.5 737.777L-24.9467 384.223L221.706 149.429L-4.79415 404.376L-358.348 757.929Z" fill="url(#paint2_linear_816_14847)" />
-            </g>
-            <g opacity="0.5" filter="url(#filter3_f_816_14847)">
-              <path d="M-190.348 749.429L-210.5 729.277L143.053 375.723L389.706 140.929L163.206 395.876L-190.348 749.429Z" fill="url(#paint3_linear_816_14847)" />
-            </g>
-            <g opacity="0.32" filter="url(#filter4_f_816_14847)">
-              <path d="M-125.848 835.929L-146 815.777L207.553 462.223L454.206 227.429L227.706 482.376L-125.848 835.929Z" fill="url(#paint4_linear_816_14847)" />
-            </g>
-          </g>
-          <defs>
-            <filter id="filter0_f_816_14847" x="383.723" y="-691.5" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
-            </filter>
-            <filter id="filter1_f_816_14847" x="215.723" y="-683" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
-            </filter>
-            <filter id="filter2_f_816_14847" x="-438.5" y="89.4292" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
-            </filter>
-            <filter id="filter3_f_816_14847" x="-270.5" y="80.9292" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
-            </filter>
-            <filter id="filter4_f_816_14847" x="-206" y="167.429" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-              {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
-            </filter>
-            <linearGradient id="paint0_linear_816_14847" x1="1033.85" y1="-621.424" x2="680.3" y2="-267.87" gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" />
-              <stop offset="1" stop-color="#D3D8DF" />
-            </linearGradient>
-            <linearGradient id="paint1_linear_816_14847" x1="865.853" y1="-612.924" x2="512.3" y2="-259.37" gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" />
-              <stop offset="1" stop-color="#D3D8DF" />
-            </linearGradient>
-            <linearGradient id="paint2_linear_816_14847" x1="-368.424" y1="747.853" x2="-14.8704" y2="394.3" gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" />
-              <stop offset="1" stop-color="#D3D8DF" />
-            </linearGradient>
-            <linearGradient id="paint3_linear_816_14847" x1="-200.424" y1="739.353" x2="153.13" y2="385.8" gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" />
-              <stop offset="1" stop-color="#D3D8DF" />
-            </linearGradient>
-            <linearGradient id="paint4_linear_816_14847" x1="-135.924" y1="825.853" x2="217.63" y2="472.3" gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" />
-              <stop offset="1" stop-color="#D3D8DF" />
-            </linearGradient>
-            <clipPath id="clip0_816_14847">
-              <rect width="1448" height="450" fill="white" transform="translate(0 -155)" />
-            </clipPath>
-          </defs>
-        </svg>
-
-      </>
-    )
-  }
+      <svg width="1328" height="295" viewBox="0 0 1328 295" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_816_14847)">
+        <g opacity="0.2" filter="url(#filter0_f_816_14847)">
+          <path d="M1023.78 -631.5L1043.93 -611.347L690.376 -257.794L443.723 -23L670.223 -277.947L1023.78 -631.5Z" fill="url(#paint0_linear_816_14847)" />
+        </g>
+        <g opacity="0.32" filter="url(#filter1_f_816_14847)">
+          <path d="M855.777 -623L875.929 -602.847L522.376 -249.294L275.723 -14.5L502.223 -269.447L855.777 -623Z" fill="url(#paint1_linear_816_14847)" />
+        </g>
+        <g opacity="0.2" filter="url(#filter2_f_816_14847)">
+          <path d="M-358.348 757.929L-378.5 737.777L-24.9467 384.223L221.706 149.429L-4.79415 404.376L-358.348 757.929Z" fill="url(#paint2_linear_816_14847)" />
+        </g>
+        <g opacity="0.5" filter="url(#filter3_f_816_14847)">
+          <path d="M-190.348 749.429L-210.5 729.277L143.053 375.723L389.706 140.929L163.206 395.876L-190.348 749.429Z" fill="url(#paint3_linear_816_14847)" />
+        </g>
+        <g opacity="0.32" filter="url(#filter4_f_816_14847)">
+          <path d="M-125.848 835.929L-146 815.777L207.553 462.223L454.206 227.429L227.706 482.376L-125.848 835.929Z" fill="url(#paint4_linear_816_14847)" />
+        </g>
+      </g>
+      <defs>
+        <filter id="filter0_f_816_14847" x="383.723" y="-691.5" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
+        </filter>
+        <filter id="filter1_f_816_14847" x="215.723" y="-683" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
+        </filter>
+        <filter id="filter2_f_816_14847" x="-438.5" y="89.4292" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
+        </filter>
+        <filter id="filter3_f_816_14847" x="-270.5" y="80.9292" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
+        </filter>
+        <filter id="filter4_f_816_14847" x="-206" y="167.429" width="720.206" height="728.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          {/* <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_816_14847" /> */}
+        </filter>
+        <linearGradient id="paint0_linear_816_14847" x1="1033.85" y1="-621.424" x2="680.3" y2="-267.87" gradientUnits="userSpaceOnUse">
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="#D3D8DF" />
+        </linearGradient>
+        <linearGradient id="paint1_linear_816_14847" x1="865.853" y1="-612.924" x2="512.3" y2="-259.37" gradientUnits="userSpaceOnUse">
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="#D3D8DF" />
+        </linearGradient>
+        <linearGradient id="paint2_linear_816_14847" x1="-368.424" y1="747.853" x2="-14.8704" y2="394.3" gradientUnits="userSpaceOnUse">
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="#D3D8DF" />
+        </linearGradient>
+        <linearGradient id="paint3_linear_816_14847" x1="-200.424" y1="739.353" x2="153.13" y2="385.8" gradientUnits="userSpaceOnUse">
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="#D3D8DF" />
+        </linearGradient>
+        <linearGradient id="paint4_linear_816_14847" x1="-135.924" y1="825.853" x2="217.63" y2="472.3" gradientUnits="userSpaceOnUse">
+          <stop stop-color="white" />
+          <stop offset="1" stop-color="#D3D8DF" />
+        </linearGradient>
+        <clipPath id="clip0_816_14847">
+          <rect width="1448" height="450" fill="white" transform="translate(0 -155)" />
+        </clipPath>
+      </defs>
+    </svg>
+    );
+  };
 
   return (
     <>
       <div className="bg-[#121212] flex justify-center items-center w-full px-3 ">
-        <div className="w-full bottom-0  space-y-6 mt-1 p-3 text-center lg:text-left text-[#D9D9D9] max-w-[1540px]">
-          <div className="relative main-footer x-sm:px-0  px-1 lg:px-20 text-left lg:flex-row justify-between lg:justify-between flex sm:flex-row x-sm:flex-col x-sm:justify-center x-sm:items-center x-sm:gap-6 items-start text-[#D9D9D9] ">
+        <div className="w-full bottom-0 space-y-6 mt-1 p-3 text-center lg:text-left text-[#D9D9D9] max-w-[1540px]">
+          <div className="relative main-footer x-sm:px-0 px-1 lg:px-20 text-left lg:flex-row justify-between lg:justify-between flex sm:flex-row x-sm:flex-col x-sm:justify-center x-sm:items-center x-sm:gap-6 items-start text-[#D9D9D9] ">
             <div className="absolute w-1/2 md:w-[90%] blur-[30px] overflow-hidden -bottom-[25px] left-[10%] pointer-events-none">
               <Glow />
             </div>
@@ -129,6 +130,7 @@ const Footer = () => {
 
                 <Link
                   to="/create"
+                  onClick={scrollToTop}
                   className="text-[#a0a0a0] font-[300] tracking-wide text-[16px] underline-none pl-1 hover:text-[#f0f0f0] hover:underline"
                 >
                   Write article
@@ -136,6 +138,7 @@ const Footer = () => {
 
                 <Link
                   to="/request"
+                  onClick={scrollToTop}
                   className="text-[#a0a0a0] font-[300] tracking-wide text-[16px] underline-none pl-1 hover:text-[#f0f0f0] hover:underline"
                 >
                   Request article
@@ -143,18 +146,21 @@ const Footer = () => {
 
                 <Link
                   to="/videos"
+                  onClick={scrollToTop}
                   className="text-[#a0a0a0] font-[300] tracking-wide text-[16px] underline-none pl-1 hover:text-[#f0f0f0] hover:underline"
                 >
                   Videos
                 </Link>
                 <Link
                   to="/story"
+                  onClick={scrollToTop}
                   className="text-[#a0a0a0] font-[300] tracking-wide text-[16px] underline-none pl-1 hover:text-[#f0f0f0] hover:underline"
                 >
                   Our story
                 </Link>
                 <Link
                   to="/team"
+                  onClick={scrollToTop}
                   className="text-[#a0a0a0] font-[300] tracking-wide text-[16px] underline-none pl-1 hover:text-[#f0f0f0] hover:underline"
                 >
                   Dev team
@@ -173,6 +179,7 @@ const Footer = () => {
 
             <Link
               to="/terms"
+              onClick={scrollToTop}
               className="hover:text-[#a0a0a0] font-[400] transition-all"
             >
               Terms of Service
--- src/components/Search/SearchPage.jsx
+++ src/components/Search/SearchPage.jsx
@@ -1,19 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo, useCallback, Suspense, lazy } from "react";
 import NavbarMini from "../Navbar/NavbarMini";
-import BlogCard from "../BlogSection/BlogCard";
 import "./SearchPage.css";
-import FilterPopUp from "../Filter/FilterPopUp";
-import Filter from "../Filter/Filter";
 import axios from "axios";
 import { BACKEND_URL } from "../../constants";
 import { useSearchParams } from "react-router-dom";
 import companyLogo from "/assets/images/company.png";
 import { ReadTime, formatDate } from "../../services/date";
+import debounce from "lodash.debounce";
 import SearchCardLoading from "./SearchCardLoading";
+
+const BlogCard = lazy(() => import("../BlogSection/BlogCard"));
+const FilterPopUp = lazy(() => import("../Filter/FilterPopUp"));
+const Filter = lazy(() => import("../Filter/Filter"));
 
 const SearchPage = () => {
   const [searchParams] = useSearchParams();
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
@@ -21,18 +23,34 @@ const SearchPage = () => {
   const [company, setCompany] = useState([]);
   const [headerName, setHeaderName] = useState("");
   const [isSearching, setIsSearching] = useState(false);
-
   const [filterPopUp, setFilterPopUp] = useState(false);
 
-  const openFilterPopup = () => {
-    setFilterPopUp(true);
-  };
+  const fetchArticles = useCallback(
+    debounce(async (query, page) => {
+      setLoading(true);
+      const params = { q: query, page, limit: 10 };
 
-  const closeFilterPopUp = () => {
-    setFilterPopUp(false);
-  };
+      try {
+        const response = await axios.get(BACKEND_URL + "/search", { params });
+        const newArticles = response.data.articles;
 
-  const fetchLatestArticles = async (endPoint, page) => {
+        if (page === 1) {
+          setArticles(newArticles);
+        } else {
+          setArticles((prevArticles) => [...prevArticles, ...newArticles]);
+        }
+
+        setHasMore(newArticles.length === 10);
+      } catch (error) {
+        console.error("Failed to fetch articles", error);
+      } finally {
+        setLoading(false);
+      }
+    }, 50),
+    []
+  );
+
+  const fetchLatestArticles = useCallback(async (endPoint, page) => {
     setLoading(true);
     try {
       const res = await axios.get(`${BACKEND_URL}${endPoint}?page=${page}`);
@@ -50,46 +68,23 @@ const SearchPage = () => {
     } finally {
       setLoading(false);
     }
-  };
+  }, []);
 
-  const fetchArticles = async (query, page) => {
-    setLoading(true);
-    const params = { q: query, page, limit: 10 };
-    console.log("params", params);
-
-    try {
-      const response = await axios.get(BACKEND_URL + "/search", { params });
-      const newArticles = response.data.articles;
-
-      if (page === 1) {
-        setArticles([...newArticles]);
-      } else {
-        setArticles(prevArticles => [...prevArticles, ...newArticles]);
-      }
-
-      setHasMore(newArticles.length === 10);
-    } catch (error) {
-      console.error("Failed to fetch articles", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const countCompany = async () => {
+  const countCompany = useCallback(async () => {
     try {
       const res = await axios.get(BACKEND_URL + "/countCompanies");
       setCompany(res.data.data);
     } catch (error) {
       console.log(error);
     }
-  };
-
-  useEffect(() => {
-    countCompany();
   }, []);
 
   useEffect(() => {
-    const query = searchParams.get('query');
+    countCompany();
+  }, [countCompany]);
+
+  useEffect(() => {
+    const query = searchParams.get("query");
     setIsSearching(!!query);
 
     if (query) {
@@ -99,53 +94,49 @@ const SearchPage = () => {
     } else {
       fetchLatestArticles("/blogs", page);
     }
-  }, [searchParams, page]);
+  }, [searchParams, page, fetchArticles, fetchLatestArticles]);
 
-  const handleShowMore = () => {
-    const query = searchParams.get('query');
+  const handleShowMore = useCallback(() => {
+    const query = searchParams.get("query");
     if (query) {
       fetchArticles(query, page + 1);
     } else {
       fetchLatestArticles("/blogs", page + 1);
     }
-    setPage(prevPage => prevPage + 1);
-  };
+    setPage((prevPage) => prevPage + 1);
+  }, [page, searchParams, fetchArticles, fetchLatestArticles]);
+
+  const openFilterPopup = useCallback(() => setFilterPopUp(true), []);
+  const closeFilterPopUp = useCallback(() => setFilterPopUp(false), []);
+
+  const headerText = useMemo(() => {
+    return headerName
+      ? headerName
+      : isSearching
+      ? decodeURIComponent(searchParams.toString().substring(6).replace(/\+/g, " "))
+      : "Latest Blogs";
+  }, [headerName, isSearching, searchParams]);
 
   return (
     <>
-      {filterPopUp && <FilterPopUp closeFilterPopUp={closeFilterPopUp} company={company} fetchArticles={fetchArticles} setHeaderName={setHeaderName} />}
+      {filterPopUp && (
+        <Suspense fallback={<div>Loading Filter...</div>}>
+          <FilterPopUp
+            closeFilterPopUp={closeFilterPopUp}
+            company={company}
+            fetchArticles={fetchArticles}
+            setHeaderName={setHeaderName}
+          />
+        </Suspense>
+      )}
       <NavbarMini />
       <div className="pt-24 px-8 md:px-4 lg:px-14 2xl:px-28 h-full">
         <div className="w-full flex gap-10 h-full">
           <div className="section-left w-full flex flex-col gap-2 h-full max-w-5xl">
             <div className="flex w-full justify-between items-center">
-              <h3 className="font-[400] text-2xl">{articles.length} Articles found for {headerName ? headerName : (isSearching ? decodeURIComponent(searchParams.toString().substring(6).replace(/\+/g, " ")) : "Latest Blogs")}</h3>
-              <svg
-                onClick={() => openFilterPopup()}
-                className="md:block hidden cursor-pointer border border-[#c1c1c1] hover:border-[#919191] transition-all rounded-lg p-[2px] w-7 h-7"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.3999 2.1001H18.5999C19.6999 2.1001 20.5999 3.0001 20.5999 4.1001V6.3001C20.5999 7.1001 20.0999 8.1001 19.5999 8.6001L15.2999 12.4001C14.6999 12.9001 14.2999 13.9001 14.2999 14.7001V19.0001C14.2999 19.6001 13.8999 20.4001 13.3999 20.7001L11.9999 21.6001C10.6999 22.4001 8.8999 21.5001 8.8999 19.9001V14.6001C8.8999 13.9001 8.4999 13.0001 8.0999 12.5001L4.2999 8.5001C3.7999 8.0001 3.3999 7.1001 3.3999 6.5001V4.2001C3.3999 3.0001 4.2999 2.1001 5.3999 2.1001Z"
-                  stroke="#616161"
-                  strokeWidth="1.5"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M10.93 2.1001L6 10.0001"
-                  stroke="#616161"
-                  strokeWidth="1.5"
-                  strokeMiterlimit="10"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+              <h3 className="font-[400] text-2xl">
+                {articles.length} Articles found for {headerText}
+              </h3>
             </div>
 
             {loading && articles.length === 0 ? (
@@ -157,37 +148,41 @@ const SearchPage = () => {
                 <SearchCardLoading />
               </>
             ) : (
-              articles.map((item) => (
-                <BlogCard
-                  key={item._id}
-                  id={item._id}
-                  link={`/blog/${item._id}`}
-                  Title={item.title}
-                  imagesrc={item.imageUrl === "your_image_url_here" ? companyLogo : item.imageUrl}
-                  author={item.author?.name}
-                  company={item.companyName}
-                  data={item.description}
-                  readingTime={ReadTime(item.description)}
-                  date={formatDate(item.createdAt)}
-                />
-              ))
+              <Suspense fallback={<div>Loading Articles...</div>}>
+                {articles.map((item) => (
+                  <BlogCard
+                    key={item._id}
+                    id={item._id}
+                    link={`/blog/${item._id}`}
+                    Title={item.title}
+                    imagesrc={item.imageUrl === "your_image_url_here" ? companyLogo : item.imageUrl}
+                    author={item.author?.name}
+                    company={item.companyName}
+                    data={item.description}
+                    readingTime={ReadTime(item.description)}
+                    date={formatDate(item.createdAt)}
+                  />
+                ))}
+              </Suspense>
             )}
 
             {hasMore && !loading && (
-              <div onClick={handleShowMore} className="pt-4 group pb-8 cursor-pointer h-full flex flex-col justify-center items-center w-full text-[#212121]">
+              <div
+                onClick={handleShowMore}
+                className="pt-4 group pb-8 cursor-pointer h-full flex flex-col justify-center items-center w-full text-[#212121]"
+              >
                 Show More
-                <svg className='rotate-90 group-hover:translate-y-2 transition-all duration-300' width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M9.42999 4L15.5 10.07L9.42999 16.14" stroke='#212121' strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="square" strokeLinejoin="round" />
-                  <path d="M4 10.0699L15 10.0699" stroke='#212121' strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="square" strokeLinejoin="round" />
-                </svg>
               </div>
             )}
 
             {loading && articles.length > 0 && <SearchCardLoading />}
-            <br /><br />
+            <br />
+            <br />
           </div>
           <div className="section-right md:hidden w-1/5 flex flex-col gap-2">
-            <Filter company={company} fetchArticles={fetchArticles} setHeaderName={setHeaderName} />
+            <Suspense fallback={<div>Loading Filter...</div>}>
+              <Filter company={company} fetchArticles={fetchArticles} setHeaderName={setHeaderName} />
+            </Suspense>
           </div>
         </div>
       </div>
