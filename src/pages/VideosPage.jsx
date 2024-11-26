bf11675ff5d765ca22519447616a278af3b0c26b
Lokendrakushwah <Lokendrakushwah8051@gmail.com>
2024-11-26 11:57:19 +0530

    Fallback

--- src/pages/VideosPage.jsx
+++ src/pages/VideosPage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Footer from "../components/Landing/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import YouTubePlaylist from "./YouTubePlaylist";
+import Videos from "./Videos";
 
 const VideosPage = () => {
   return (
     <div className="flex h-screen flex-col">
       <Navbar className="sticky top-0 z-50 bg-white" />
       <div className="flex-grow">
-        <YouTubePlaylist />
+        {/* <YouTubePlaylist /> */}
+        <Videos />
       </div>
       <Footer />
     </div>
