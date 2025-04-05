3d78251225593f2e54879b4d0b68bba8102f9829
Harshal patil <91362856+Harshal141@users.noreply.github.com>
2025-04-05 12:16:06 +0530

    Update BACKEND_URL to use staging environment fallback

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "https://oss-backend.vercel.app/api/anubhav";
-// export const BACKEND_URL = 'http://localhost:3000/api/anubhav';
+const fallbackURL = "https://oss-backend-staging.vercel.app/api/anubhav";
+
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || fallbackURL;
 export const YOUTUBE_PLAYLIST =
   "https://www.youtube.com/watch?v=c58BoAV2dPc&list=PL4REmU9xh1P14Qv0zmwjve_myZI_rOguU&index=";
